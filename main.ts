16199150affa4a1b5a9004c22550a757c4e5e92a
Hampe007 <100205514+Hampe007@users.noreply.github.com>
2022-02-25 09:32:43 +0100

    test

--- main.ts
+++ main.ts
@@ -11,35 +11,7 @@ let B = 0
 let A = 0
 let speed = 0
 radio.setGroup(1)
-music.setVolume(250)
 music.playMelody("C5 A F E D G B F ", 240)
-basic.showIcon(IconNames.Square)
-basic.clearScreen()
-basic.showIcon(IconNames.Diamond)
-basic.clearScreen()
-basic.showIcon(IconNames.SmallSquare)
-basic.clearScreen()
-basic.showIcon(IconNames.SmallDiamond)
-basic.clearScreen()
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-basic.clearScreen()
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . # # # .
-    . # . # .
-    . . . . .
-    `)
-basic.clearScreen()
-basic.showIcon(IconNames.Yes)
-basic.clearScreen()
-music.setVolume(0)
 music.stopAllSounds()
 basic.forever(function () {
     bitbot.select_model(BBModel.Classic)
@@ -50,22 +22,30 @@ basic.forever(function () {
         bitbot.setPixelColor(0, 0x00FF00)
         bitbot.setPixelColor(1, 0x00FF00)
         bitbot.setPixelColor(2, 0x00FF00)
-        bitbot.move(BBMotor.Right, BBDirection.Forward, speed)
+        bitbot.setPixelColor(3, 0x00FF00)
+        bitbot.setPixelColor(4, 0x00FF00)
+        bitbot.move(BBMotor.Left, BBDirection.Forward, speed)
     } else {
         bitbot.setPixelColor(0, 0xFF0000)
         bitbot.setPixelColor(1, 0xFF0000)
         bitbot.setPixelColor(2, 0xFF0000)
-        bitbot.move(BBMotor.Right, BBDirection.Forward, 0)
+        bitbot.setPixelColor(3, 0xFF0000)
+        bitbot.setPixelColor(4, 0xFF0000)
+        bitbot.move(BBMotor.Left, BBDirection.Forward, 0)
     }
     if (B == 1) {
         bitbot.setPixelColor(6, 0x00FF00)
         bitbot.setPixelColor(7, 0x00FF00)
         bitbot.setPixelColor(8, 0x00FF00)
-        bitbot.move(BBMotor.Left, BBDirection.Forward, speed)
+        bitbot.setPixelColor(9, 0x00FF00)
+        bitbot.setPixelColor(10, 0x00FF00)
+        bitbot.move(BBMotor.Right, BBDirection.Forward, speed)
     } else {
         bitbot.setPixelColor(6, 0xFF0000)
         bitbot.setPixelColor(7, 0xFF0000)
         bitbot.setPixelColor(8, 0xFF0000)
-        bitbot.move(BBMotor.Left, BBDirection.Forward, 0)
+        bitbot.setPixelColor(9, 0xFF0000)
+        bitbot.setPixelColor(10, 0xFF0000)
+        bitbot.move(BBMotor.Right, BBDirection.Forward, 0)
     }
 })
